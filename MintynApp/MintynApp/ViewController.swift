880201d3fbca813c45a8222e0e5bdd7313f36a7a
brightsummit.co <naseemoyebola@gmail.com>
2022-03-11 22:25:38 +0100

    Initial Commit

---
+++ MintynApp/MintynApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MintynApp
+//
+//  Created by IBE on 11/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
