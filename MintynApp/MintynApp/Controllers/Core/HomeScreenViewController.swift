6df55f2a5378456110700c4bf379f44b9af9aefb
brightsummit.co <naseemoyebola@gmail.com>
2022-03-12 20:40:19 +0100

    Add extra Screen for Firebase Email and Password Authentication

---
+++ MintynApp/MintynApp/Controllers/Core/HomeScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreenViewController.swift
+//  MintynApp
+//
+//  Created by naseem on 12/03/2022.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+
+}
